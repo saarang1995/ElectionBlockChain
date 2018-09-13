45e1d0f39b7eec764d72104e878002ee10540772
saarang1995 <saarang.tiwari@hotmail.com>
2018-09-13 11:42:37 +0530

    Added candidates in the Election.sol

--- contracts/election.sol
+++ contracts/election.sol
@@ -1,11 +1,40 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Store Candidate
-    // Read Candidate
-    string public candidate;
-    // Consrtuctor
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store Candidates 
+    //Fetch Candidate
+
+    /** 
+    (using mapping function -> which is solidity function just like any
+     other language's key-map function)
+    **/
+
+    mapping(uint => Candidate) public candidates;        //soring and fetching
+    
+    /**
+    the above will add a candidate to the mapping, which will change the state
+     of the Contract and writing to the blockchain. This will be interacting with
+     the data layer of the blockchain.
+    **/
+
+    //Store Candidates Count
+    uint public candidateCount;
+
+
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+
+    }
+
+    function addCandidate(string _name) private {       // made it private so that only the contract can add the candidate
+        candidateCount ++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
